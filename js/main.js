e1dc88204c15b285f27e16b78e77413161829ec1
Fabien <cacrfabfr@gmail.com>
2021-10-26 11:33:10 +0200

    module1

--- js/main.js
+++ js/main.js
@@ -15,29 +15,67 @@
 //   yoyoEase: 'none'
 // })
 
+const runStart = () => {
+  console.log('onStart');
+}
 
-gsap.fromTo(
-  'body',
-  { backgroundColor : '#fff'},
-  { backgroundColor : '#76c897', duration: 2, opacity: 1 })
+const runUpdate = () => {
+  console.log('Update'); // You can use this for loading bar
+}
 
-gsap.fromTo(
-  'h1',
-  { y: -40, opacity: 0 },
-  { y: 0, duration: 1, delay: 1, opacity: 1 })
+const runComplete = () => {
+  console.log('Complete');
+}
 
-gsap.fromTo('.intro',
-  { y: -40, opacity: 0 },
-  { y: 0, opacity: 1, delay: 1.8, duration: 1 })
+const timeline = gsap.timeline({ 
+  duration: .7,
+  paused: true,
+  onStart: runStart,
+  onUpdate: runUpdate,
+  onComplete: runComplete,
+});
 
-gsap.fromTo('img',
-  { y: -40, opacity: 0 },
-  { y: 0, opacity: 1, delay: 2.5, duration: 1 })
 
-gsap.fromTo('h2',
-  { y: -40, opacity: 0 },
-  { y: 0, opacity: 1, delay: 3.5, duration: 1 })
+timeline.fromTo(
+    'body',
+    { backgroundColor : '#fff'},
+    { backgroundColor : '#76c897', duration: 2, opacity: 1 })
+  .fromTo(
+    'h1',
+    { y: -40, opacity: 0 },
+    { y: 0, opacity: 1 }, '-=1')
+  .fromTo('.intro',
+    { y: -40, opacity: 0 },
+    { y: 0, opacity: 1 }, '-=0.5')
+  .fromTo('img',
+    { y: -40, opacity: 0 },
+    { y: 0, opacity: 1 })
 
-gsap.fromTo('ul',
-{ y: -40, opacity: 0 },
-{ y: 0, opacity: 1, delay: 4.5, duration: 1 })
+  .fromTo('h2',
+    { y: -40, opacity: 0 },
+    { y: 0, opacity: 1 })
+  .fromTo('ul',
+    { y: -40, opacity: 0 },
+    { y: 0, opacity: 1 })
+
+
+    const playButton = document.getElementById('btnPlay');
+    const pauseButton = document.getElementById('btnPause');
+    const resumeButton = document.getElementById('btnResume');
+    const reverseButton = document.getElementById('btnReverse');
+    const speedUpButton = document.getElementById('btnSpeedUp');
+    const slowDownButton = document.getElementById('btnSlowDown');
+    const seekButton = document.getElementById('btnSeek');
+    const progressButton = document.getElementById('btnProgress');
+    const restartButton = document.getElementById('btnRestart');
+
+    playButton.addEventListener('click', () => timeline.play());
+    pauseButton.addEventListener('click', () => timeline.pause());
+    resumeButton.addEventListener('click', () => timeline.resume());
+    reverseButton.addEventListener('click', () => timeline.reverse());
+    speedUpButton.addEventListener('click', () => timeline.timeScale(2));
+    slowDownButton.addEventListener('click', () => timeline.timeScale(0.5));
+    seekButton.addEventListener('click', () => timeline.seek(2));
+    progressButton.addEventListener('click', () => timeline.progress(0.5));
+    restartButton.addEventListener('click', () => timeline.restart());
+    
