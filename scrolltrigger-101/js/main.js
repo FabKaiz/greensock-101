fcbe0de84db26c3b0f69e28ad54b4156a3e91de4
Fabien <cacrfabfr@gmail.com>
2021-10-26 13:11:05 +0200

    fade in on scroll

--- scrolltrigger-101/js/main.js
+++ scrolltrigger-101/js/main.js
@@ -2,13 +2,13 @@ gsap.registerPlugin(ScrollTrigger);
 
 function init(){
   //fade in the scroll trigger
-  gsap.to('#intro img', { opacity: 0, scrollTrigger: {
-    trigger: '#intro',
-    start: 'top top',
-    end: 'bottom center',
-    scrub: true,
-    // markers: true,
-  } });
+  // gsap.to('#intro img', { opacity: 0, scrollTrigger: {
+  //   trigger: '#intro',
+  //   start: 'top top',
+  //   end: 'bottom center',
+  //   scrub: true,
+  //   // markers: true,
+  // } });
 
   // toggle css class on scroll
   gsap.set('#project02', {scrollTrigger: {
@@ -26,13 +26,42 @@ function init(){
       trigger: '.bcg-parallax',
       start: 'top bottom-=20%',
       scrub: true,
-      markers: true,
+      // markers: true,
     }
   });
 
   parallaxTl
     .from('.content-wrapper', {duration: 0.4, autoAlpha: 0}, 0.4)
-    .from('.bcg', {duration: 6, y: '-50%'}, 0)
+    .from('.bcg', {duration: 6, y: '-40%'}, 0)
+
+  // Pin 
+  gsap.to(['#intro h1', '#intro p'], {
+    autoAlpha: 0,
+    ease: 'none',
+    scrollTrigger: {
+      trigger: '#intro .content',
+      start: 'top top',
+      pin: true,
+      scrub: true,
+      // markers: true,
+    }
+  })
+
+  // Fade in
+  const projects = document.querySelectorAll('.project');
+  projects.forEach(project => {
+    gsap.from(project, {
+      opacity: 0,
+      yPercent: 5,
+      scrollTrigger: {
+        trigger: project.querySelector('img'),
+        start: 'top bottom-=300',
+        end: 'top center',
+        toggleActions: 'play none none reverse',
+        markers: true,
+      }
+    })
+  })
 }
 
 window.addEventListener('load', function(){
